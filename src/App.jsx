28c63e23a117e9be74a890a65ff1fc98d434481d
Sajinimalsha <2021is021@stu.ucsc.cmb.ac.lk>
2024-05-25 21:40:53 +0530

    side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Sidebar />
       
-    </div>
+
+
+
+
+    </>
   )
 }
 
